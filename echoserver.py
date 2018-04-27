86589638d90023aca5549344bc302546277530cf
Fia <rafia.anwar95@gmail.com>
2018-04-26 21:08:33 -0400

    verification fix

--- echoserver.py
+++ echoserver.py
@@ -8,15 +8,14 @@ PAT = 'EAAFDVWHFI8gBAO66TFhEEzhO3hgCeJcR4Mbl6uFZAAGED2asUFUDlSoDYQliguJQHzQQ1bUP
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-  print("Handling Verification.")
-  print(request.get_data())
-  print(request.args)
-  print(*request.args)
-  if request.values["hub.verify_token"] == 'my_voice_is_my_password_verify_me':
-    print("Verification successful!")
+  print "Handling Verification."
+  payload = request.get_data()
+  print(payload)
+  if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
+    print "Verification successful!"
     return request.args.get('hub.challenge', '')
   else:
-    print("Verification failed!")
+    print "Verification failed!"
     return 'Error, wrong validation token'
 
 @app.route('/', methods=['POST'])
