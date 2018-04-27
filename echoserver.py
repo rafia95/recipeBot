cbc33c80aa30d89438bf5f3399d5b87dcdeb33f2
Fia <rafia.anwar95@gmail.com>
2018-04-26 20:49:44 -0400

    verification fix

--- echoserver.py
+++ echoserver.py
@@ -13,6 +13,7 @@ def handle_verification():
   print("Handling Verification.")
   print(request.get_data())
   print(request.args)
+  print(request.values)
   if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
     print("Verification successful!")
     return request.args.get('hub.challenge', '')
