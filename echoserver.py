782577efa10beed1a102ec2e36a181f79f1773d0
Fia <rafia.anwar95@gmail.com>
2018-04-28 22:45:44 -0400

    changing template

--- echoserver.py
+++ echoserver.py
@@ -12,7 +12,29 @@ r = http.request('GET', 'http://www.tastespotting.com/browse/2')
 # import beautifulsoup to parse data
 from bs4 import BeautifulSoup
 
-
+response = requests.post(
+    "https://graph.facebook.com/v2.6/me/messenger_profile?access_token="+PAT,
+    json={
+          "persistent_menu":[
+                            {
+                               "locale":"default",
+                               "composer_input_disabled": true,
+                               "call_to_actions":[
+                                                  {
+                                                    "type":"web_url",
+                                                    "title":"Latest News",
+                                                    "url":"https://www.messenger.com/",
+                                                    "webview_height_ratio":"full"
+                                                   }
+												  ]
+                            },
+                            {
+                               "locale": "zh_CN",
+                               "composer_input_disabled": False
+                            }
+                            ]
+                             })
+          
 
 @app.route('/', methods=['GET'])
 def handle_verification():
