ec9a07fc229b9b4be9c8c2ceb572f166674edfd6
Fia <rafia.anwar95@gmail.com>
2018-07-23 22:12:29 -0400

    add count

--- echoserver.py
+++ echoserver.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 PAT = 'EAADJQYB3nKABAK2F9MCFRG86MOEcNlQ2Nbm7TSPmWvZA9ZAx4xQ4nrLIiVzVY9Qf9FYKeEuE5NkNOWmk64bd2EYVCixlqbdBLKOELZANtfZARcG2NXLrQD9lawAkDAXZBTLnd2yM3Ux9rTYrv95W0KAuNFciYvL1ZCie3DeTipswZDZD'
 http = urllib3.PoolManager()
 r = http.request('GET', 'http://www.tastespotting.com/browse/1')
-count = 0
 # import beautifulsoup to parse data
 from bs4 import BeautifulSoup
 response = requests.post("https://graph.facebook.com/v2.6/me/thread_settings?access_token="+PAT,
@@ -91,27 +90,17 @@ def retrieving_data():
     print("url ",url)
     req = http.request('GET', url)
     data = BeautifulSoup(req.data,'html.parser')
-    #creating array
-    array=[]
-    i=0
     for each_div in data.find_all("div", { "class": "trendspotted-item"}):
-        array.append([])
         for each_recipe in each_div.find_all('a', href=True):
             """links starting with /clicks are the links of recipe to their original sites, so just retrieve those links"""
             if each_recipe['href'].startswith('/click'):
                 retrieving_data.recipe_link=each_recipe['href'][16:-12]
-                array[i].append(retrieving_data.recipe_link)
                # print("the recipe_link is ----------",retrieving_data.recipe_link,each_recipe['href'])
             for each_img in each_recipe.find_all('img', alt=True):
                 retrieving_data.msg2=each_img['src']
-                array[i].append(retrieving_data.msg2)
         for each_caption in each_div.find("p", { "class": "photo_caption"}):
             retrieving_data.msg3=each_caption
-            array[i].append(retrieving_data.msg3)
-        print("i is",i)
-        print("calling increment counter method")
-        increment_counter(i)
-    #print("ARRAY",array)
+ 
 			
 def send_message(token, recipient, text):
       """Send the message text to recipient with id recipient.
@@ -155,9 +144,6 @@ def send_message(token, recipient, text):
       headers={'Content-type': 'application/json'})
      # if r.status_code != requests.codes.ok:
      #    print(r.text)
-def increment_counter(j):
-    global count
-    count = j + 1
-    print ("count ",count)
+
 if __name__ == '__main__':
   app.run()
