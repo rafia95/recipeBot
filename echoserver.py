e6474d385e1fea8b28dcea7cb0805ed687b0ab08
Fia <rafia.anwar95@gmail.com>
2018-07-29 18:36:07 -0400

    postback type

--- echoserver.py
+++ echoserver.py
@@ -73,12 +73,9 @@ def messaging_events(payload):
   data = json.loads(payload)
   messaging_events = data["entry"][0]["messaging"]
   for event in messaging_events:
-    if (event.type == "postback"):
-      if (event.postback.payload == "send_recipe_payload"):
-         yield event["sender"]["id"], "send back a rec"
-    else:
      if "message" in event and "text" in event["message"]:
-       yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+       if event["message"]["text"] == "send_recipe_payload":
+        yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
      else:
        yield event["sender"]["id"], "I can't echo this"
 
