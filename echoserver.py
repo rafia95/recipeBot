e4687fa29e28caad748d92b2bfb3b796f682fa2b
Fia <rafia.anwar95@gmail.com>
2018-08-23 19:36:48 -0400

    changed key

--- echoserver.py
+++ echoserver.py
@@ -73,7 +73,7 @@ def handle_messages():
   payload = request.get_data()
   print(payload)
   appkey = "myappkey007"
-  digester = hmac.new(appkey,payload,hashlib.sha1)
+  digester = hmac.new(b'appkey',payload,hashlib.sha1)
   generated_signature = "sha1="+digester.hexdigest()
   print (generated_signature)
   signature = request.headers.get("X-Hub-Signature")
